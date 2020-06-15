6cf50c3b9cb938d9bba1b16bc584c894a6905cee
Ruoxi Ren <ruoxiren_1007@163.com>
2020-06-14 21:57:35 -0400

    add search suggestions

--- web_project/scripts/select.js
+++ web_project/scripts/select.js
@@ -3,4 +3,47 @@
 //     $('.mdb-select').materialSelect();
 //     });
 
+
 $('select').selectpicker();
+
+$( function() {
+    var courses = ["ME","EE","COMP"];
+    $( "#search-bar-input" ).autocomplete({
+      source: courses
+    });
+} );
+
+
+// $('#search-bar-input').mdbAutocomplete({
+//     data: courses
+// });
+
+// $( function() {
+//     var availableTags = [
+//       "ActionScript",
+//       "AppleScript",
+//       "Asp",
+//       "BASIC",
+//       "C",
+//       "C++",
+//       "Clojure",
+//       "COBOL",
+//       "ColdFusion",
+//       "Erlang",
+//       "Fortran",
+//       "Groovy",
+//       "Haskell",
+//       "Java",
+//       "JavaScript",
+//       "Lisp",
+//       "Perl",
+//       "PHP",
+//       "Python",
+//       "Ruby",
+//       "Scala",
+//       "Scheme"
+//     ];
+//     $( "#search-bar-input" ).autocomplete({
+//       source: availableTags
+//     });
+//   } );
