993039be112718af637cb43ff6b1ee24ae34544a
Shang Wang <wangshang19950911@gmail.com>
2020-07-29 15:47:06 -0400

    add logo image to the navbar

--- web_project/src/components/navigation/index.js
+++ web_project/src/components/navigation/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 // import { Navbar, Nav } from "react-bootstrap";
-import { Navbar } from "react-bootstrap";
+import { Image, Navbar } from "react-bootstrap";
 import SearchBar from "./SearchBar";
 import './styles.css';
+
+import logoPic from "../logo/logo-2.png";
 
 
 // function UserOption() {
@@ -21,7 +23,17 @@ import './styles.css';
 function NavigationBar(props) {
   return (
     <Navbar bg="none" expand="lg" variant="none" fixed="top">
-      <Navbar.Brand href="/">Halligan Reviews</Navbar.Brand>
+      <Navbar.Brand href="/">
+        <Image src={logoPic} alt="logo" className="logo-img" />
+        {/* <img
+        alt=""
+        src="../logo/logo-1.png"
+        width="30"
+        height="30"
+        className="navbar-logo"
+      />{' '} */}
+        Halligan Reviews
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
       <Navbar.Collapse id="responsive-navbar-nav">
         <SearchBar onUpdate={props.onUpdate} />
